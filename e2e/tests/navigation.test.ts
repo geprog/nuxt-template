efdcee0acf6f5a57fa58195cb54b2a729c21cece
hmathavan <h.mathavan@geprog.com>
2025-06-12 08:18:25 +0000

    setup e2e and add test job in pipeline

---
+++ e2e/tests/navigation.test.ts
@@ -0,0 +1,16 @@
+
+import { expect, test } from '@nuxt/test-utils/playwright';
+
+test.describe('Page Navigation', () => {
+  test('should navigate to the Start page on click', async ({ page }) => {
+    await page.goto('/');
+    await page.click('text="Start page"');
+    await page.screenshot({ path: './e2e/screenshots/navigation-start-page.png' });
+  });
+  test('should navigate to the Other page on click', async ({ page }) => {
+    await page.goto('/');
+    await page.click('text="Other page"');
+    await expect(page).toHaveURL('/other');
+    await page.screenshot({ path: './e2e/screenshots/navigation-other-page.png' });
+  });
+});
---
+++ playwright.config.ts
@@ -0,0 +1,18 @@
+import type { ConfigOptions } from '@nuxt/test-utils/playwright';
+import process from 'node:process';
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig<ConfigOptions>({
+  testDir: './e2e/tests',
+  timeout: 120_000,
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: 'line',
+  testMatch: '**/*.test.ts',
+  use: {
+    video: 'off',
+    trace: 'off',
+  },
+
+});
