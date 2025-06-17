8597377d86028145563b172bba5079fa7512401a
mariusheine <m.heine@geprog.com>
2025-06-17 09:32:21 +0000

    properly assert against screenshots

--- e2e/tests/navigation.test.ts
+++ e2e/tests/navigation.test.ts
@@ -4,12 +4,14 @@ test.describe('Page Navigation', () => {
   test('should navigate to the Start page on click', async ({ page }) => {
     await page.goto('/');
     await page.click('text="Start page"');
-    await page.screenshot({ path: './e2e/screenshots/navigation-start-page.png' });
+    await expect(page).toHaveURL('/');
+    await expect(page).toHaveScreenshot('start-page.png');
   });
+
   test('should navigate to the Other page on click', async ({ page }) => {
     await page.goto('/');
     await page.click('text="Other page"');
     await expect(page).toHaveURL('/other');
-    await page.screenshot({ path: './e2e/screenshots/navigation-other-page.png' });
+    await expect(page).toHaveScreenshot('other-page.png');
   });
 });
