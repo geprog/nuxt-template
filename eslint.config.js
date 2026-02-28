389a0a5c27a7f3badea390004223f1f5145334a1
Marius Heine <m.heine@geprog.com>
2026-02-28 10:01:16 +0100

    Update deps (#45)
    * update dependencies
    
    * update node version

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,37 @@
 import antfu from '@antfu/eslint-config';
 
-export default antfu({
-  vue: true,
-  typescript: true,
-  formatters: {
+export default antfu(
+  {
+    vue: true,
+    typescript: true,
+    formatters: {
     /**
      * Format CSS, LESS, SCSS files, also the `<style>` blocks in Vue
      * By default uses Prettier
      */
-    css: true,
-    /**
-     * Format HTML files
-     * By default uses Prettier
-     */
-    html: true,
-    /**
-     * Format Markdown files
-     * Supports Prettier and dprint
-     * By default uses Prettier
-     */
-    markdown: 'prettier',
+      css: true,
+      /**
+       * Format HTML files
+       * By default uses Prettier
+       */
+      html: true,
+      /**
+       * Format Markdown files
+       * Supports Prettier and dprint
+       * By default uses Prettier
+       */
+      markdown: 'prettier',
+    },
+    rules: {
+      'style/semi': ['error', 'always'],
+    },
   },
-  rules: {
-    'style/semi': ['error', 'always'],
-    'vue/block-order': ['error', {
-      order: [['template', 'script'], 'style'],
-    }],
+  {
+    files: ['**/*.vue'],
+    rules: {
+      'vue/block-order': ['error', {
+        order: [['template', 'script'], 'style'],
+      }],
+    },
   },
-});
+);
